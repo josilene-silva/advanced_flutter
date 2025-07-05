08e7b6d08f5e2abaad38d6438a38e5f2c4c5852e
Josilene Silva <josilenevitoriasilva@gmail.com>
2025-07-05 15:24:58 -0400

    change strategy to use factory constructor

--- test/domanin/entities/next_event_player_test.dart
+++ test/domanin/entities/next_event_player_test.dart
@@ -3,24 +3,45 @@ import 'package:flutter_test/flutter_test.dart';
 class NextEventPlayer {
   final String id;
   final String name;
-  late final String initials;
+  final String initials;
   final String? photo;
   final String? position;
   final bool isConfirmed;
   final DateTime? confirmationDate;
 
-  NextEventPlayer({
+  // construtor privado
+  NextEventPlayer._({
     required this.id,
     required this.name,
+    required this.initials,
     required this.isConfirmed,
     this.photo,
     this.position,
     this.confirmationDate,
-  }) {
-    initials = getInitials();
-  }
+  });
 
-  String getInitials() {
+  // factory constructor
+  // O factory constructor também chama o método _getInitials
+  // para obter as iniciais do nome do jogador, que são armazenadas na propriedade initials
+  // do objeto NextEventPlayer.
+  factory NextEventPlayer({
+    required String id,
+    required String name,
+    required bool isConfirmed,
+    String? photo,
+    String? position,
+    DateTime? confirmationDate,
+  }) => NextEventPlayer._(
+    id: id,
+    name: name,
+    isConfirmed: isConfirmed,
+    initials: _getInitials(name),
+    photo: photo,
+    position: position,
+    confirmationDate: confirmationDate,
+  );
+
+  static String _getInitials(String name) {
     final names = name.split(' ');
     final firstChar = names.first[0];
     final lastChar = names.last[0];
