b70a75b7868c81743557187ceb5c005e51340a4b
Josilene Silva <josilenevitoriasilva@gmail.com>
2025-09-20 15:51:56 -0400

    move HttpGetClient to its own file

--- test/infra/api/repositories/load_next_event_api_repository_test.dart
+++ test/infra/api/repositories/load_next_event_api_repository_test.dart
@@ -1,6 +1,7 @@
 import 'package:advanced_flutter/domain/entities/next_event.dart';
 import 'package:advanced_flutter/domain/entities/next_event_player.dart';
 import 'package:advanced_flutter/domain/repositories/load_next_event_repository.dart';
+import 'package:advanced_flutter/infra/api/clients/http_get_client.dart';
 import 'package:advanced_flutter/infra/types/json.dart';
 
 import 'package:flutter_test/flutter_test.dart';
@@ -44,10 +45,6 @@ class NextEventPlayerMapper {
     position: json['position'],
     confirmationDate: DateTime.tryParse(json['confirmationDate'] ?? ''),
   );
-}
-
-abstract class HttpGetClient {
-  Future<T> get<T>({required String url, Json? params});
 }
 
 class HttpGetClientSpy implements HttpGetClient {
